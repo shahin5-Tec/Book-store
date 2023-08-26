773471fb19c6d98207cef1643a8f545d83d77672
Shahin Hossain <shahiniubat99@gmail.com>
2023-08-26 19:41:43 +0600

    add the navbar active item

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,55 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { BoltIcon } from '@heroicons/react/24/solid'
-
+import { Link, NavLink } from "react-router-dom";
+import { BoltIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100">
-        <Link to="/" className=" flex items-center"></Link>
-        {/* logo-part */}
-        <BoltIcon className="h-6 w-6 text-blue-500" />
-        <span className="font-bold ms-3  text-xl">nextPage</span>
-
+    <div className="  bg-gray-100  px-4 py-5 container mx-auto lg:px-8 lg:max-w-screen-xl">
+      <div className="flex items-center justify-between">
         
+          {/* logo-part */}
+          <Link to="/" className="flex items-center ">
+            <BoltIcon class="h-6 w-6 text-blue-500" />
+            <span className="text-xl ms-2 font-bold -tracking-wide">
+              nextPage
+            </span>
+          </Link>
+       
+        <div>
+          <ul className="lg:flex  items-center space-x-8 font-semibold">
+          <li >
+                  <NavLink
+                    to={'/'}
+                    className={({ isActive }) =>isActive ? " text-blue-600": ""}         
+                    
+                  >
+                    Home
+                   
+                  </NavLink>
+                </li>
+          <li >
+                  <NavLink
+                    to={'/books'}
+                    className={({ isActive }) =>isActive ? "  text-blue-600": ""}         
+                    
+                  >
+                    Books
+                   
+                  </NavLink>
+                </li>
+          <li >
+                  <NavLink
+                    to={'/about'}
+                    className={({ isActive }) =>isActive ? "  text-blue-600": ""}         
+                    
+                  >
+                    About us
+                   
+                  </NavLink>
+                </li>
+           
+          </ul>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,31 +4,8 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-        
-"primary": "#3ce047",
-        
-"secondary": "#235793",
-        
-"accent": "#c6ce54",
-        
-"neutral": "#2a213b",
-        
-"base-100": "#eae0f0",
-        
-"info": "#71b9f4",
-        
-"success": "#75ebba",
-        
-"warning": "#a75f06",
-        
-"error": "#ec3b27",
-        },
-      },
-    ],
+  theme: {
+    extend: {},
   },
   plugins: [require("daisyui")],
 }
