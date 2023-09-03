c34e0ef28e0f4998a09ffe68df79982676029373
Shahin Hossain <shahiniubat99@gmail.com>
2023-09-03 15:07:05 +0600

    done the read-more and read-less button

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -9,7 +9,8 @@ const BookDetails = () => {
     bookDetails;
   return (
     <div className="my-container ">
-      <div className="flex mx-28 border">
+      <div className="flex flex-col lg:flex-row overflow-hidden mx-28 border">
+        {/* image */}
         <div className="lg:w-1/2 h-full">
           <img
             src={bookDetails.image}
@@ -17,9 +18,9 @@ const BookDetails = () => {
             className="object-cover mt-0 w-full lg:h-full"
           />
         </div>
-
-        <div className="mx-8 mt-10 ">
-          <p className="bg-yellow-400 font-semibold uppercase mb-4 w-48 rounded-full pl-2 mt-8 ">
+        {/* details-container */}
+        <div className="p-8 lg:p-16 lg:w-1/2 ">
+          <p className="bg-yellow-400 font-semibold uppercase mb-4 w-48 rounded-full pl-2  ">
             BRAND NEW
           </p>
           <p className="text-black font-bold text-3xl">{bookDetails.title}</p>
@@ -27,11 +28,29 @@ const BookDetails = () => {
           <p className="mb-3">Publisher: {bookDetails.publisher}</p>
           <p className="mb-3">Year: {bookDetails.year}</p>
           <p className="mb-3">Rating: {bookDetails.rating}</p>
-          <p className="mb-5">{bookDetails.desc.substring(0, 50)}...
-           
+         {
+          fold?(
+            <>
+            <p>{bookDetails.desc.substring(0,100)}...
+            <span className="text-blue-600 cursor-pointer font-semiold
+            " onClick={()=>setFold(!fold)}>
+              Read More
+            </span>
             </p>
+            </>
+          ):(
+            <>
+            <p>{bookDetails.desc}
+            <span className="text-blue-600 cursor-pointer font-semiold
+            " onClick={()=>setFold(!fold)}>
+              Read Less
+            </span>
+            </p>
+            </>
+          )
+         }
           <div className="inline-flex items-center">
-            <span className="text-white font-semibold inline-flex items-center mt-5 px-6  btn bg-blue-400 hover:bg-blue-700 transition duration-200">
+            <span className="text-white font-semibold inline-flex items-center mt-3 px-6  btn bg-blue-400 hover:bg-blue-700 transition duration-200">
               <p>Buy Now</p>
             </span>
             <p className="mt-3 ms-3 font-bold text-xl">
